10aff221a8e65724fe85d67791a5f88c4e6d5cff
James Roose <jroose9@gmail.com>
2019-02-09 16:59:54 -0800

    Fill in calculate_metric function with ppv, npv, sens, spec calculations and binomial inference

--- R/metricCompare.R
+++ R/metricCompare.R
@@ -17,27 +17,61 @@ compare_metric <- function(predictions1, predictions2, labels, comparison,
   NULL
 }
 
-calculate_metric <- function(predictions, labels, comparison, metric,
-                             threshold_type, threshold,  confidence = 0.95){
+calculate_metric <- function(predictions,
+                             labels,
+                             metric = "ppv",
+                             threshold_type,
+                             threshold,
+                             inference,
+                             confidence = 0.95){
   #Check Inputs
   #If using total positive type, find threshold in terms of score
-  if (threshold_type = "prob"){
+  if (threshold_type == "prob"){
     prob_threshold = threshold
-  } else if (threshold_type = "total_pos"){
+  } else if (threshold_type == "total_pos"){
     prob_threshold = quantile(predictions, 1 - threshold)
   } else {stop("Invalid threshold type specified; must be one of prob, total_pos")}
-  pos_preds = as.numeric(predictions > threshold)
-  neg_preds = as.numeric(predictions < threshold)
+  pos_preds = as.numeric(predictions > prob_threshold)
+  neg_preds = as.numeric(predictions < prob_threshold)
   # Confusion Matrix
-  TP = sum(pos_preds*labels)
+  TP = sum(pos_preds*(labels==1))
   FP = sum(pos_preds) - TP
   TN = sum(neg_preds*(labels==0))
   FN = sum(neg_preds) - TN
-  # Get Requested Metrics
+  # And derived confusion matrix
   ppv = TP/(TP + FP)
   npv = TN/(TN + FN)
   sens = TP/(TP + FN)
   spec = TN/(TN + FP)
-  # Standard Error Estimates
-  NULL
+
+  # Get Requested Metric
+  if ( !(metric %in% c("ppv", "npv", "sens", "spec") ) ){
+    stop("Invalid metric specified, must be one of ppv, npv, sens, spec")
+  }
+
+  # Standard Error Estimates - all of these metrics are in 0/1, one option is
+  # to assume they are a binomial parameter and use usual binomial SE calculation
+  z = -qnorm((1-confidence)/2)
+  n = length(labels)
+  p = get(metric)
+  se = NULL
+  if (inference  == "binomial"){
+    se = get_binomial_se(n, p)
+  } else if (inference == "logit_binomial"){
+    se = NULL
+  } else {stop("Invalid inference type specified; must be one of binomial,
+               logit_binomial")
+  }
+  return(list(metric, est = p, var = se^2, se = se, ci_l = p - z*se, ci_u = p + z*se))
 }
+
+get_binomial_se <- function(n, p){
+  sqrt((1/n)*p*(1-p))
+}
+
+#Some testing code
+calculate_metric(predictions = X,
+                 labels = Y, metric = "sens",
+                 threshold_type = "prob",
+                 threshold = .75,
+                 inference = "binomial")
