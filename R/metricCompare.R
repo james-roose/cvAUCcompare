46629eaff753b05db2865191deda12bf8289b3d9
James Roose <jroose9@gmail.com>
2019-02-09 16:38:03 -0800

    Fill in confusion matrix calculation

--- R/metricCompare.R
+++ R/metricCompare.R
@@ -14,14 +14,30 @@ compare_metric <- function(predictions1, predictions2, labels, comparison,
   #If using total positive type, find threshold in terms of score
   #Calculate Confusion Matrix and Metrics of Interest
   #Get Comparisons and Inference via Delta Method
-NULL
+  NULL
 }
 
 calculate_metric <- function(predictions, labels, comparison, metric,
                              threshold_type, threshold,  confidence = 0.95){
   #Check Inputs
   #If using total positive type, find threshold in terms of score
-  #Calculate Confusion Matrix and Metrics of Interest
-  #Get Metric and Inference
-NULL
+  if (threshold_type = "prob"){
+    prob_threshold = threshold
+  } else if (threshold_type = "total_pos"){
+    prob_threshold = quantile(predictions, 1 - threshold)
+  } else {stop("Invalid threshold type specified; must be one of prob, total_pos")}
+  pos_preds = as.numeric(predictions > threshold)
+  neg_preds = as.numeric(predictions < threshold)
+  # Confusion Matrix
+  TP = sum(pos_preds*labels)
+  FP = sum(pos_preds) - TP
+  TN = sum(neg_preds*(labels==0))
+  FN = sum(neg_preds) - TN
+  # Get Requested Metrics
+  ppv = TP/(TP + FP)
+  npv = TN/(TN + FN)
+  sens = TP/(TP + FN)
+  spec = TN/(TN + FP)
+  # Standard Error Estimates
+  NULL
 }
