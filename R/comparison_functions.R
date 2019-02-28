cfcaab120a193dc2e911d82fc827505d62f00076
James Roose <jroose9@gmail.com>
2019-02-28 10:35:13 -0800

    Add input checks on all 3 functions.

--- R/comparison_functions.R
+++ R/comparison_functions.R
@@ -15,12 +15,14 @@
 #'
 #' @examples
 delta_method <- function(grad, Sigma){
+  stopifnot(is.Matrix(Sigma))
   t(grad)%*%Sigma%*%grad
 }
 
 # Difference
 # Function to Compute the Difference and the variance of the difference
 difference <- function(psi1, psi2, ic1, ic2){
+  .check_inputs(psi1, psi2, ic1, ic2)
   n_obs = length(ic1)
   h = psi1 - psi2
   #Gradient of h(psi1, psi2) and Covariance Matrix of ICs
@@ -34,6 +36,7 @@ difference <- function(psi1, psi2, ic1, ic2){
 # Ratio
 # Function to Compute the Ratio in and the variance of the Ratio
 ratio <- function(psi1, psi2, ic1, ic2){
+  .check_inputs(psi1, psi2, ic1, ic2)
   n_obs = length(ic1)
   h = psi1/psi2
 
@@ -48,6 +51,7 @@ ratio <- function(psi1, psi2, ic1, ic2){
 # Log-Ratio
 # Function to Compute the log-ratio and the variance of the log ratio
 logratio <- function(psi1, psi2, ic1, ic2){
+  .check_inputs(psi1, psi2, ic1, ic2)
   n_obs = length(ic1)
   h = log(psi1) - log(psi2)
 
@@ -57,4 +61,12 @@ logratio <- function(psi1, psi2, ic1, ic2){
 
   var_h = (1/nobs)*delta_method(grad, Sigma)
   return(list(h = h, var_h = var_h))
+}
+
+# Function to check inputs for 3 IC based comparison functions
+.check_inputs <- function(psi1, psi2, ic1, ic2) {
+  stopifnot(is.numeric(psi1) & (psi1 < 1) & (psi1 > 0))
+  stopifnot(is.numeric(psi2) & (psi2 < 1) & (psi2 > 0))
+  stopifnot(is.numeric(ic1) & (is.numeric(ic2)) &
+              length(ic1 == ic2) & (ic1 != ic2))
 }
