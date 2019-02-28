de06b12d050ad337798558f4f2c25e280f728ceb
James Roose <jroose9@gmail.com>
2019-02-28 10:14:29 -0800

    Initial commit of skeleton.

---
+++ R/cvAUPRC_ic.R
@@ -0,0 +1,20 @@
+source("./R/internals.R")
+
+#' Function to calculate influence function for cvAUPRC estimate.
+#'
+#' @param predictions1
+#' @param labels
+#' @param label.ordering
+#' @param folds
+#' @param confidence
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cvAUPRC_ic <- function(predictions1, labels, label.ordering = NULL, folds = NULL,
+                       confidence = 0.95) {
+
+    #Placeholder for later cvAUPRC calc
+    return(NULL)
+  }
